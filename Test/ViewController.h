accf1cc5bbe726a77a3158f07f668f82fbe03a68
Gruber Kristóf <gk@lka.hu>
2014-09-11 18:20:11 +0200

    Initial version

---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by GK Work on 14.09.11..
+//  Copyright (c) 2014 dms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test
+//
+//  Created by GK Work on 14.09.11..
+//  Copyright (c) 2014 dms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
